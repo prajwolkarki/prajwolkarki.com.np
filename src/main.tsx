1876796906d5beb6939ef3fde0309a0b94354d57
prajwolkarki <prajwolkarki609@gmail.com>
2025-04-25 23:34:36 +0545

    feat:lyang

--- src/main.tsx
+++ src/main.tsx
@@ -3,40 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes.tsx";
-import AnimatedCursor from "react-animated-cursor";
-
-const isDesktop = window.innerWidth > 768;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <>
       <RouterProvider router={router} />
-      {isDesktop && (
-        <AnimatedCursor
-          innerSize={12}
-          outerSize={30}
-          color="193, 11, 111"
-          outerAlpha={0.3}
-          innerScale={0.8}
-          outerScale={3}
-          trailingSpeed={8}
-          clickables={[
-            'a',
-            'button',
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            'label[for]',
-            'select',
-            'textarea',
-            '.link',
-            '.cursor-pointer'
-          ]}
-        />
-      )}
     </>
   </StrictMode>
 );
-
