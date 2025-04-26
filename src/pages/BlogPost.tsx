aa5062565600e1919a866442e3836a2db6a2dc39
prajwolkarki <prajwolkarki609@gmail.com>
2025-04-26 13:41:50 +0545

    fixed cors error

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,10 +1,12 @@
-import { useParams } from "react-router"
+import useGetBlogBySlug from "@/hooks/useGetBlogBySlug";
+import { useParams } from "react-router";
 
 const BlogPost = () => {
-    const { slug } = useParams();
-  return (
-    <div>{slug}</div>
-  )
-}
+  const { slug } = useParams();
+  const { data: post, isPending, isError, error } = useGetBlogBySlug(slug);
+  if (isPending) return <div>Loading.....</div>;
+  if (isError) return <div>Error:{error.message}</div>;
+  return <div>{slug}{JSON.stringify(post)}</div>;
+};
 
-export default BlogPost
+export default BlogPost;
--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -17,7 +17,7 @@ const Blogs = () => {
   
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">Blogs</h1>
+      <h1 className="text-3xl font-bold mb-8 font-tagesschrift underline">Blogs</h1>
       <div className="flex flex-col gap-4">
         {posts?.map((post:any) => (
           <BlogCard
